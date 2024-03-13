1abe82bcd6dc029a2461b0a7921fdeaf0ef1e13b
ccdf846153 <2337791509@qq.com>
2024-03-13 10:11:21 +0800

    asd

--- content/seminar/read.js
+++ content/seminar/read.js
@@ -1,20 +1,13 @@
 function read() {
-    var request = new XMLHttpRequest();
-    // request.setHeader("Access-Control-Allow-Origin", "*");
-    // request.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    // request.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
-    request.open(
-        'GET', 
-        'https://gitee.com/ccdf846153/unic_resources/tree/master/content.html', 
-        true
-    );
-    request.onreadystatechange = function() {
-        if (request.readyState === 4) {
-            if (request.status === 200 || request.status === 0) {
-                var content = request.responseText;
-                console.log(content);
-            }
-        }
-    };
-    request.send(null);
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+
+    // 传参并指定回调执行函数为onBack
+    script.src = 'https://gitee.com/ccdf846153/unic_resources/tree/master/content.html&callback=onBack';
+    document.head.appendChild(script);
+
+    // 回调执行函数
+    function onBack(res) {
+        console.log(JSON.stringify(res));
+    }
 }
