0c76ff5c751ac1decc64ee5ee2c78c66556b2837
ccdf846153 <2337791509@qq.com>
2024-03-13 09:38:17 +0800

    Update read.js

--- content/seminar/read.js
+++ content/seminar/read.js
@@ -1,6 +1,10 @@
 function read() {
     var request = new XMLHttpRequest();
-    request.open('GET', './content.html', true);
+    request.open(
+        'GET', 
+        'https://github.com/ccdf846153/starter-hugo-research-group/tree/main/content/seminar/content.html', 
+        true
+    );
     request.onreadystatechange = function() {
         if (request.readyState === 4) {
             if (request.status === 200 || request.status === 0) {
