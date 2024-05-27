2451ed25f03d64d4b168da9cb6c0fda995ebf28d
ccdf846153 <2337791509@qq.com>
2024-05-27 22:54:52 +0800

    asd

--- content/seminar/update_content_html.py
+++ content/seminar/update_content_html.py
@@ -243,6 +243,7 @@ def material_item_html(material_tuple):
                 </a>
             </p>
         """
+    width = 93 - 8 * len(button_name_list)
     
     return (TABLE_ITEM := f"""
     <tr class="{year_class}">
@@ -265,7 +266,7 @@ def material_item_html(material_tuple):
             <p style="display: inline-block; margin: 0; margin-left: 1.5rem;">
             </p>
             {material_html_str}
-            <p style="text-align: left; display: inline-block; vertical-align: middle;">
+            <p style="text-align: left; display: inline-block; max-width: {width}%; vertical-align: middle;">
                 <span>{presentation_title}</span>
             </p>
         </td>
