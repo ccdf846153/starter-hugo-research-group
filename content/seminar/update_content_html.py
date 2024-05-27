6914aefa707fdf0268b870d40eb719770229e048
ccdf846153 <2337791509@qq.com>
2024-05-27 22:49:17 +0800

    asd

--- content/seminar/update_content_html.py
+++ content/seminar/update_content_html.py
@@ -262,6 +262,8 @@ def material_item_html(material_tuple):
             <p style="display: inline-block; margin: 0;"
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </p>
+            <p style="display: inline-block; margin: 0; margin-left: 1.5rem;">
+            </p>
             {material_html_str}
             <p style="text-align: left; display: inline-block; width: 85%; vertical-align: middle;">
                 <span>{presentation_title}</span>
