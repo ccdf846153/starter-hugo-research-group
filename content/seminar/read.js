a2260df76468187af040965924dbc68ff7924ceb
ccdf846153 <2337791509@qq.com>
2024-11-09 14:10:20 +0800

    Update read.js

--- content/seminar/read.js
+++ content/seminar/read.js
@@ -1,12 +1,12 @@
 async function read() {
     try {
-        var response = await fetch('https://unicxidian.netlify.app/seminar_list/content.txt');
+        var response = await fetch('https://www.unicxidian.org/seminar_list/content.txt');
         var fileContent = await response.text();
 
-        if (fileContent == undefined) {
-            response = await fetch('https://unicxidian.org/seminar_list/content.txt');
-            fileContent = await response.text();
-        }
+        // if (fileContent == undefined) {
+        //     response = await fetch('https://unicxidian.org/seminar_list/content.txt');
+        //     fileContent = await response.text();
+        // }
 
         const parser = new DOMParser();
         const doc = parser.parseFromString(fileContent, 'text/html');
